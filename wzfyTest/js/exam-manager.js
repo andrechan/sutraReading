fe08ed0007be1a7d550c6881ae7817685a8d6b1a
andrechan <andrechan2020@gmail.com>
2025-12-04 09:12:35 +0800

    Update exam-manager.js

--- wzfyTest/js/exam-manager.js
+++ wzfyTest/js/exam-manager.js
@@ -1,4 +1,4 @@
-// exam-manager.js
+// 考试流程管理器
 class ExamManager {
     constructor(questionLoader, resultCalculator, historyManager) {
         this.questionLoader = questionLoader;
@@ -11,8 +11,10 @@ class ExamManager {
         this.examTimer = null;
         this.timeRemaining = 60 * 60;
         this.timeLimit = 60 * 60;
+        this.questionCount = 10;
     }
     
+    // 开始考试
     async startExam(questionCount) {
         this.questionCount = questionCount;
         this.questions = await this.questionLoader.loadQuestions(questionCount);
@@ -29,6 +31,7 @@ class ExamManager {
         return this.questions;
     }
     
+    // 开始计时器
     startTimer() {
         this.updateTimerDisplay();
         
@@ -42,6 +45,7 @@ class ExamManager {
         }, 1000);
     }
     
+    // 更新计时器显示
     updateTimerDisplay() {
         const timerElement = document.getElementById('time-remaining');
         if (timerElement) {
@@ -49,10 +53,12 @@ class ExamManager {
         }
     }
     
+    // 选择答案
     selectAnswer(questionIndex, answerIndex) {
         this.userAnswers[questionIndex] = answerIndex;
     }
     
+    // 下一题
     nextQuestion() {
         if (this.currentQuestionIndex < this.questions.length - 1) {
             this.currentQuestionIndex++;
@@ -61,6 +67,7 @@ class ExamManager {
         return false;
     }
     
+    // 上一题
     previousQuestion() {
         if (this.currentQuestionIndex > 0) {
             this.currentQuestionIndex--;
@@ -69,6 +76,7 @@ class ExamManager {
         return false;
     }
     
+    // 完成考试
     finishExam() {
         clearInterval(this.examTimer);
         
@@ -82,10 +90,12 @@ class ExamManager {
         return results;
     }
     
+    // 获取当前题目
     getCurrentQuestion() {
         return this.questions[this.currentQuestionIndex];
     }
     
+    // 获取进度信息
     getProgress() {
         return {
             current: this.currentQuestionIndex + 1,
@@ -93,11 +103,36 @@ class ExamManager {
         };
     }
     
+    // 检查是否是最后一题
     isLastQuestion() {
         return this.currentQuestionIndex === this.questions.length - 1;
     }
     
+    // 检查是否是第一题
     isFirstQuestion() {
         return this.currentQuestionIndex === 0;
+    }
+    
+    // 获取用户答案
+    getUserAnswer(questionIndex) {
+        return this.userAnswers[questionIndex];
+    }
+    
+    // 检查是否所有题目都已作答
+    isExamCompleted() {
+        return this.userAnswers.every(answer => answer !== null);
+    }
+    
+    // 重置考试状态
+    resetExam() {
+        this.questions = [];
+        this.currentQuestionIndex = 0;
+        this.userAnswers = [];
+        this.timeRemaining = this.timeLimit;
+        
+        if (this.examTimer) {
+            clearInterval(this.examTimer);
+            this.examTimer = null;
+        }
     }
 }
